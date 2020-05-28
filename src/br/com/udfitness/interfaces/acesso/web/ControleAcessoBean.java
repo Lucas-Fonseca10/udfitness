15f5c5646b208c4e1497e2559377e9e32ba8d984
Lucas Alves <lucas-fonseca10@hotmail.com>
2020-05-28 15:13:20 -0300

    Acrescentada lógica de permissão de entrada

--- src/br/com/udfitness/interfaces/acesso/web/ControleAcessoBean.java
+++ src/br/com/udfitness/interfaces/acesso/web/ControleAcessoBean.java
@@ -8,8 +8,11 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.udfitness.application.service.AcessoService;
+import br.com.udfitness.application.service.AlunoService;
 import br.com.udfitness.application.util.ValidationException;
 import br.com.udfitness.domain.acesso.TipoAcesso;
+import br.com.udfitness.domain.aluno.Aluno;
+import br.com.udfitness.domain.aluno.Aluno.Situacao;
 
 @Named
 @RequestScoped
@@ -17,35 +20,44 @@ public class ControleAcessoBean {
 
 	@EJB
 	private AcessoService acessoService;
-	
+
+	@EJB
+	private AlunoService alunoService;
+
 	@Inject
 	private FacesContext facesContext;
-	
+
 	private String matricula;
 	private String cpf;
-	
+
 	public String registrarAcesso() {
 		TipoAcesso tipoAcesso;
 		try {
-		tipoAcesso = acessoService.registrarAcesso(matricula,cpf);
-		} catch(ValidationException e){
+			Aluno alunoVerificar = alunoService.findByMatricula(matricula);
+			if (alunoVerificar != null && alunoVerificar.getSituacao() == Situacao.Inativo || alunoVerificar.getSituacao() == Situacao.Pendente) {
+				facesContext.addMessage(null, new FacesMessage(alunoVerificar.getSituacao() + ", registro negado"));
+				return null;
+			}
+			tipoAcesso = acessoService.registrarAcesso(matricula, cpf);
+
+		} catch (ValidationException e) {
 			facesContext.addMessage(null, new FacesMessage(e.getMessage()));
 			return null;
 		}
-		
+
 		String msg;
-		if(tipoAcesso== TipoAcesso.Entrada) {
-			msg="ENTRADA registrada!";
-		}else if (tipoAcesso== TipoAcesso.Saida) {
-			msg= "SAÍDA registrada!";
-		}else {
-			msg="Dados de registro de acesso inconscistentes";
+		if (tipoAcesso == TipoAcesso.Entrada) {
+			msg = "ENTRADA registrada!";
+		} else if (tipoAcesso == TipoAcesso.Saida) {
+			msg = "SAÍDA registrada!";
+		} else {
+			msg = "Dados de registro de acesso inconscistentes";
 		}
-		
-		facesContext.addMessage(null,new FacesMessage(msg));
+
+		facesContext.addMessage(null, new FacesMessage(msg));
 		return null;
 	}
-	
+
 	public String getMatricula() {
 		return matricula;
 	}
@@ -62,5 +74,4 @@ public class ControleAcessoBean {
 		this.cpf = cpf;
 	}
 
-	
 }
